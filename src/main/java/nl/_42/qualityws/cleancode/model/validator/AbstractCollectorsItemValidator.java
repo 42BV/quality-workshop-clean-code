e75fa1eec37454eacf72934b6f999434ae409c57
Bas de Vos <bas.de.vos@42.nl>
2017-06-26 10:07:45 +0200

    Made implementation method of AbstractCollectorsItemValidator final.

--- src/main/java/nl/_42/qualityws/cleancode/model/validator/AbstractCollectorsItemValidator.java
+++ src/main/java/nl/_42/qualityws/cleancode/model/validator/AbstractCollectorsItemValidator.java
@@ -2,17 +2,17 @@ package nl._42.qualityws.cleancode.model.validator;
 
 import java.util.List;
 
-import nl._42.qualityws.cleancode.model.CollectorsItem;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import nl._42.qualityws.cleancode.model.CollectorsItem;
 
 public abstract class AbstractCollectorsItemValidator<T extends CollectorsItem> implements CollectorsItemValidator<T> {
 
     protected final static Logger LOGGER = LoggerFactory.getLogger(AbstractCollectorsItemValidator.class);
 
     @Override
-    public boolean validate(T item) {
+    public final boolean validate(T item) {
         List<ValidationError> errors = validateItem(item);
         if (!errors.isEmpty()) {
             logErrorHeader(item);
